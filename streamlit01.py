066f6d5ef44357c01bc80cf97849c80c62addcdc
Prakit Junkhum <prakit340@hotmail.com>
2023-09-17 11:34:24 +0700

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -84,14 +84,14 @@ if st.button("ทำนายผล"):
    
    out = Knn_model.predict(x_input)
 
+   st.button("ไม่ทำนายผล")
+
    if out[0] == 'Setosa':
-    st.image("./pic/iris1.jpg")
+        st.image("./pic/iris1.jpg")
    elif out[0] == 'Versicolor':       
-    st.image("./pic/iris2.jpg")
+        st.image("./pic/iris2.jpg")
    else:
-    st.image("./pic/iris3.jpg")
-
-    st.button("ไม่ทำนายผล")
-
+        st.image("./pic/iris3.jpg")
+        
 else:
     st.button("ไม่ทำนายผล")
