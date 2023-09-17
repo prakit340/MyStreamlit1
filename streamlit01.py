a0b273b7662b1cb5ea4718c93e127e9439f6b49e
Prakit Junkhum <prakit340@hotmail.com>
2023-09-17 10:58:27 +0700

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 st.title("การทดสอบการเขียน Website ด้วย Streamlit")
+st.header("Prakit Junkhum")
+st.subheader("My Streamlit 01")
+
 col1, col2 = st.columns(2)
 #col1.write("Col1")
 #col2.write("Col2")
@@ -13,13 +16,17 @@ with col2:
     st.markdown("มหาวิทยาลัยราชภัฏนครปฐม")
     
 html_1 = """
-<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#77c900;padding:15px 15px 15px 15px;margin:30px 30px 30px 30px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>Mr.Prakit Junkhum</h5></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
 
-st.header("Prakit Junkhum")
-st.subheader("My Streamlit 01")
 
-st.image('./pic/123456.jpg')
+colx1, colx2, colx3 = st.columns(3)
+with colx1:
+    st.image('./pic/123456.jpg')
+with colx2:
+    st.image('./pic/123456.jpg')
+with colx3:
+    st.image('./pic/123456.jpg')
