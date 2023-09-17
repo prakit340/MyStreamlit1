95e5a81a1547e96036b6c64ea671494184c1ee99
Prakit Junkhum <prakit340@hotmail.com>
2023-09-17 10:37:59 +0700

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -2,8 +2,12 @@ import streamlit as st
 
 st.title("การทดสอบการเขียน Website ด้วย Streamlit")
 col1, col2 = st.columns(2)
-col1.write("Col1")
-col2.write("Col2")
+#col1.write("Col1")
+#col2.write("Col2")
+with col1:
+    st.header("Prakit Junkhum")
+with col2:
+    st.image('./pic/123456.jpg')
 
 st.header("Prakit Junkhum")
 st.subheader("My Streamlit 01")
