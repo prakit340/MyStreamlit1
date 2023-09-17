e41930501fc9802cf8dcfa1eef3b4f079ada09e5
Prakit Junkhum <prakit340@hotmail.com>
2023-09-17 10:44:17 +0700

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -5,9 +5,14 @@ col1, col2 = st.columns(2)
 #col1.write("Col1")
 #col2.write("Col2")
 with col1:
-    st.header("Prakit Junkhum")
-with col2:
     st.image('./pic/123456.jpg')
+with col2:
+    
+    st.header("คุณประกฤต จันทร์ขำ")
+    st.subheader("บริษัท ไทยคอนโทรลโซลูชั่น จำกัด")
+    st.subheader("กาญจนบุรี")
+    st.subheader("มหาวิทยาลัยราชภัฏนครปฐม")
+    
 
 st.header("Prakit Junkhum")
 st.subheader("My Streamlit 01")
